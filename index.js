52337e20c8c07c8535d4abe123301672df9070f5
Robert Delaney <71196819+doggybootsy@users.noreply.github.com>
2021-12-23 13:16:24 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,42 +3,40 @@ const electron = require("electron")
 const Module = require("module")
 
 electron.app.commandLine.appendSwitch("no-force-async-hooks-checks")
-
+// New BrowserWindow constructor
 class BrowserWindow extends electron.BrowserWindow {
   constructor(opt) {
     if (!opt || !opt.webPreferences || !opt.webPreferences.preload || !opt.title) return super(opt)
     const originalPreload = opt.webPreferences.preload
     process.env.DISCORD_PRELOAD = originalPreload
-    
-    opt.webPreferences = Object.assign(opt.webPreferences, {
-      contextIsolation: false,
-      enableRemoteModule: true,
-      nodeIntegration: true,
-      preload: join(__dirname, "preload.js")
-    })
+
+    opt.webPreferences.contextIsolation = false
+    opt.webPreferences.enableRemoteModule = true
+    opt.webPreferences.preload = true
+    opt.webPreferences.preload = join(__dirname, "preload.js")
     super(opt)
   }
 }
-
+// Kill CSP
 electron.app.once("ready", () => {
   electron.session.defaultSession.webRequest.onHeadersReceived(function({ responseHeaders }, callback) {
     for (const iterator of Object.keys(responseHeaders))
       if (iterator.includes("content-security-policy"))
         delete responseHeaders[iterator]
-        
+
     callback({ 
       cancel: false, 
       responseHeaders
     })
   })
 })
-
+// New electron
 const Electron = new Proxy(electron, { get: (target, prop) => prop === "BrowserWindow" ? BrowserWindow : target[prop] })
-
+// Replace electron with new proxy
 const electronPath = require.resolve("electron")
 delete require.cache[electronPath].exports
 require.cache[electronPath].exports = Electron
-
+// Load Discord's asar
 const basePath = join(process.resourcesPath, "app.asar")
 const pkg = require(join(basePath, "package.json"))
 electron.app.setAppPath(basePath)
